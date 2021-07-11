5510cfaf6633dd2b55df6f4d62999f3c0512891f
Sacha <sacha.athias@epita.fr>
2021-07-11 12:46:55 +0200

    Update; bonjour by aurevoir

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Bonjour les amis");
-    println!("Bonjour les amis");
+    println!("Aurevor les amis");
     println!("add line");
     println!("wesh stars");
     println!("Hello, world!");
